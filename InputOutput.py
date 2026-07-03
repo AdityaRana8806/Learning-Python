c30099349843d9ad09a2968dc9075473cf6e2969
AdityaRana8806 <adityarana886@gail.com>
2026-07-03 07:46:22 +0530

    Basics and Foundation of Python

---
+++ InputOutput.py
@@ -0,0 +1,10 @@
+# Name = input("What is your name : ")
+# age = input("What is your Age : ")
+# print(f"Your Name is {Name} and your Age is {age}")
+
+#OR
+#print("Your name is", Name, "and Your Age is", age)
+
+a = 12.8
+b = 40
+print(int(a+b))
---
+++ Strings.py
@@ -0,0 +1,19 @@
+String = "College"
+
+#String Indexing
+print (String)         #Output will be
+print (String[0])      #College
+print (String[1])      #C
+print (String[2])      #o
+print (String[3])      #l
+print (String[4])      #l
+print (String[5])      #e
+print (String[6])      #g
+
+#String Slicing
+#print(String[Start Point : End point +1 : Steps])
+
+print(String[0:7:2])
+
+
+#Output of print(String[0]) = Output of print(String[-7])                 
