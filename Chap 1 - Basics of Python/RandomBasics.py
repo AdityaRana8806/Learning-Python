c963dd10637066c0ec50f253c408a5b8b4b07b4d
AdityaRana8806 <adityarana886@gail.com>
2026-08-27 08:17:22 +0530

    Introductions and Basic Changes

---
+++ Chap 1 - Basics of Python/RandomBasics.py
@@ -0,0 +1,30 @@
+A = 23
+B=0
+B+=A
+A+=B
+print(A)
+print(type(A))
+
+i = 2
+f = 4.3324
+d = 3.4
+c = 3 + 8j
+name = "Aditya Rana"
+boul = True
+boul2 = False
+o = None
+print(i, d, d, c, name, boul, boul2, o)
+print(type(i))
+print(type(f))
+print(type(d))
+print(type(c))
+print(type(name))
+print(type(boul))
+print(type(o))
+
+print("Hello How are you")
+name = input("Enter your Name : ")
+age = int(input("Enter you Age : "))
+print(f"Hello your Name is {name} and your Age is {age}")
+
+  
